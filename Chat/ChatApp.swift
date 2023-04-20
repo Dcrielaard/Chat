5ce912556fc420caa2a6358cb6368a5daa4a7991
Dennis Crielaard <denniscrielaard@icloud.com>
2023-04-20 17:05:20 +0200

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Dennis Crielaard on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
